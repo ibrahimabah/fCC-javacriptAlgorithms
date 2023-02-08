3a97fca97de6f81be305f29bea57cd68d83eef97
Ibrahima Bah <bibrahim97@outlook.com>
2023-02-08 19:30:27 +0300

    working caesars cipher

---
+++ caesars-cipher.js
@@ -0,0 +1,48 @@
+//Caesars Cipher is a Caesars method of shifting letters to encrypt a message from enemies
+// Rot13 means rating the caesars cipher 13 places
+//
+
+/*
+
+    rot13 = (str) => 
+    
+    letters : "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K" "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+
+    reference = "Hello"
+    accumulator = " "
+
+*/
+
+function rot13(str) {
+
+    // log letters in an object
+    const letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+    // create letterChecker
+    let letterChecker = "";
+    
+    // loop through the string
+    for (let i = 0; i < str.length; i++){
+
+        // capture each letter
+        const char = str[i];
+        const isLetter = letters.includes(char);
+
+        // if char is not letter, add to letCk
+        if (isLetter === false){
+            letterChecker += char;
+        } else {
+
+        // else, rotate + or - 13, add to letCk
+            // find the index of that letter (where is it position in the letters)
+            const mainChar = letters.findIndex((c) => c === char);
+
+            // add 13 to that letter or remove 13 from it
+            letterChecker += letters[mainChar + 13] || letters[mainChar - 13];
+        }
+    }        
+    // return letCk
+    return letterChecker;
+}
+
+rot13("SERR PBQR PNZC")
