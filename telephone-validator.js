dc6f84d8428303291f64a7785ec136e062159b43
Ibrahima Bah <bibrahim97@outlook.com>
2023-02-11 00:29:08 +0300

    ...

--- telephone-validator.js
+++ telephone-validator.js
@@ -1,2 +1,35 @@
 // Telephone Number Validator
 // CHALLENGE : validate or reject US telephone numbers based provided formats
+
+/*
+    
+ */
+
+function telephoneCheck(str) {
+    const valTel = [
+      // 555-555-5555
+      /^\d{3}-\d{3}-\d{4}$/,
+  
+      //  1 555-555-5555
+      /^1 \d{3}-\d{3}-\d{4}$/,
+  
+      //  1 (555) 555-5555
+      /^1 \(\d{3}\) \d{3}-\d{4}$/,
+  
+      //  5555555555
+      /^\d{10}$/,
+  
+      //  (555)555-5555
+      /^\(\d{3}\)\d{3}-\d{4}$/,
+  
+      //  1(555)555-5555
+      /^1\(\d{3}\)\d{3}-\d{4}$/,
+  
+      //  1 555 555 5555
+      /^1 \d{3} \d{3} \d{4}$/
+    ]
+    return valTel.some((telephone) => telephone.test(str));
+  }
+  
+  console.log(telephoneCheck("555-555-5555"));
+  
